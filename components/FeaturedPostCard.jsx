4b34a3540b32efcabc5720cc53c139d5f2c2c7c3
Andrew Castro <acastro@blackberry.com>
2022-02-27 15:49:10 -0500

    finishing comment component

---
+++ components/FeaturedPostCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeaturedPostCard() {
+  return (
+    <div>FeaturedPostCard</div>
+  )
+}
+
+export default FeaturedPostCard
---
+++ sections/FeaturedPosts.jsx
@@ -0,0 +1,1 @@
+// Make a GetFeaturedPosts graphql call
